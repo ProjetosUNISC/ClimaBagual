6949846c1cc4e1c9f26fb45efa1f95806c254ef6
Mateus <mateushaetinger37@gmail.com>
2025-06-15 11:54:49 -0300

    adicionando projeto java maven, add mysql conec e add FlatLaf dependecies

---
+++ ClimaBagual/src/main/java/com/mycompany/climabagual/ClimaBagual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.climabagual;
+
+/**
+ *
+ * @author Mateus
+ */
+public class ClimaBagual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
